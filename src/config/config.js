ad910be1ec8d4b8b1accfba5c37c2e956c1e175a
sarthakvijayvargiya <vsarthakvijay@gmail.com>
2024-06-10 00:05:29 +0530

    chore: added url and ids required for backend connection

---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+export const config = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECTID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+};
